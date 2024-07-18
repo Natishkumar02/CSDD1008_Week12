070bee9f8a67326635b40386bf53846656bd6305
Natishkumar02 <168783269+Natishkumar02@users.noreply.github.com>
2024-07-18 19:36:58 -0400

    main.go

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
-func Addition(a, b int) int {
-	return a + b
+func Factorial(n int) int {
+	if n < 0 {
+		return -1
+	}
+	result := 1
+	for i := 1; i <= n; i++ {
+		result *= i
+	}
+	return result
 }
